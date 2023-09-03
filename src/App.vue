75a4eab2b2b1421b7bba96e219389a3ab8e51b5c
Logan <841471723@qq.com>
2023-09-03 21:53:34 +0800

    scss 文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
   <!-- 一级路由出口 -->
   <RouterView />
+  <div class="text">test text</div>
 </template>
+
+<style scoped lang="scss">
+  .text{
+    color: $priceColor;
+  }
+</style>
