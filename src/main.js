68d661d7c612cade6264fc211fd0228a05302655
Logan <841471723@qq.com>
2023-09-05 21:37:25 +0800

    图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
@@ -15,3 +16,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // le:指令绑定的那个元素
+        // binding binding.value = 指令等于号后面绑定的表达式的值 图片url
+        console.log('el', el, binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                if (isIntersecting) {
+                    el.src = binding.value
+                }
+                console.log('isIntersecting', isIntersecting)
+            }
+        )
+    }
+})
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
+// import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -15,10 +15,10 @@ import HomePanel from './components/HomePanel.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <HomePanel title="新鲜好物" sub-title="新鲜好物啊啊啊啊">
+  <!-- <HomePanel title="新鲜好物" sub-title="新鲜好物啊啊啊啊">
     111
   </HomePanel>
   <HomePanel title="人气推荐" sub-title="人气推荐啊啊啊啊">
     22
-  </HomePanel>
+  </HomePanel> -->
 </template>
